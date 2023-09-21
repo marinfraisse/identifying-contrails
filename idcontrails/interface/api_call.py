ee165aa80a3ab9b04430d8acc78ecb1ec4a0801f
Marin Fraisse <marin.fraisse@hotmail.com>
2023-09-21 16:24:46 +0200

    URL changée, dockerfile updaté

--- idcontrails/interface/api_call.py
+++ idcontrails/interface/api_call.py
@@ -2,10 +2,11 @@ import requests
 import numpy as np
 
 url_cloud_arthur_0 = 'https://contrails-2pojbkqtxa-ew.a.run.app'
+url_cloud_projet_contrails = 'https://contrails-yxi6ktszda-uc.a.run.app'
 
 def api_call_predict(X) :
     X_bytes = X.tobytes()
-    result = requests.post(url_cloud_arthur_0 + "/upload_image/", files= {"img" : X_bytes} )
+    result = requests.post(url_cloud_projet_contrails + "/upload_image/", files= {"img" : X_bytes} )
     print(f"status_code is : {result.status_code}")
     X_mask_pred = np.frombuffer(result.content, dtype = np.float32).reshape(256,256,1)
     return X_mask_pred
